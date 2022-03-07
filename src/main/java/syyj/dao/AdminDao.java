50b229cafcaf644141d3f799a8939cb8b3e240c3
shaonian <76216210+shaonianjzh@users.noreply.github.com>
2022-03-07 20:58:16 +0800

    second version

--- src/main/java/syyj/dao/AdminDao.java
+++ src/main/java/syyj/dao/AdminDao.java
@@ -1,8 +1,9 @@
 package syyj.dao;
 
+import org.apache.ibatis.annotations.Param;
 import syyj.domain.Admin;
 
 public interface AdminDao {
 
-    Admin find(String username,String password);
+    Admin find(@Param("username") String username, @Param("password") String password);
 }
